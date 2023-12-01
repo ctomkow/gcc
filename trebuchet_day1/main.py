783eacbf306542c4777867ad92c3a6c217b5aed4
Gualberto <gdtlumauig001@gmail.com>
2023-12-01 23:16:13 +0000

    main.py

--- trebuchet_day1/main.py
+++ trebuchet_day1/main.py
@@ -27,7 +27,12 @@ import sys
 # # todo: combine dicts from parse left and right and do math
 # input: dict(left), dict(right)
 # print final int total
+#test
 
 with open(os.path.join(sys.path[0],"starmap"),'r') as txtlines:
     lines = txtlines.readlines()
-
+    
+    for line in lines:
+        line = line.strip()
+        print(line)
+        y=input()
