43470a1762174becc95f29b92cb59eab9281f904
ctomkow <ctomkow@gmail.com>
2023-12-01 15:01:42 -0700

    skeletor

---
+++ trebuchet_day1/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# # function
+# # todo: open file
+# # todo: read by line break
+# # main loop
+# # feed into paring functions
+# # take outputs and feed into combining function for result
+#
+# # function
+# # todo: parse from left
+# input: str(line)
+# output: dict
+#     - key: line number
+#     - value: str(int) found
+#
+# # function
+# # todo: parse from right
+# input: str(line)
+# output: dict
+#     - key: line number
+#     - value: str(int) found
+#
+# # function
+# # todo: combine dicts from parse left and right and do math
+# input: dict(left), dict(right)
+# print final int total
