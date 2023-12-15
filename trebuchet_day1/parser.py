ffe565189d4d413e216b20226690c8f05456a37e
ctomkow <ctomkow@gmail.com>
2023-12-15 15:27:13 -0700

    fix

--- trebuchet_day1/parser.py
+++ trebuchet_day1/parser.py
@@ -10,6 +10,7 @@ def parse_from_left(line: str) -> str:
     for s in line:
         if s in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             return s
+    return ''
 
 
 print(parse_from_left(sample_data))
